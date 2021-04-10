9e6cd569539e51fa74ea5a140c492f25d716895d
linerocks <tirtadwipa.manunggal@gmail.com>
2021-04-10 14:12:28 +0700

    update

--- src/container/Home/api.ts
+++ src/container/Home/api.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.43.13:8000';
+const API_BASE_URL = 'https://ec2-3-238-181-74.compute-1.amazonaws.com:8000';
 export const ruok = () => {
   return axios.get(`${API_BASE_URL}/ruok`);
 };
